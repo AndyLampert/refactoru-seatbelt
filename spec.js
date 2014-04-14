bfbf4c1de2224ce2c1e38cc97f46dc9972cb93dd
AndyLampert <lamperta@gmail.com>
2014-04-14 12:04:10 -0600

    finished peer programming exercise with Bjorn

---
+++ spec.js
@@ -0,0 +1,13 @@
+// Unit Testing (spec)
+
+// should return true
+// 4 % 2 --> 0
+assert(isEven(4), true);
+
+// should return false
+assert(isEven(3), false);
+
+// should return true
+assert(isEven(0), true);
+
+// assert(pluralize('cat', 0), 'cats');
---
+++ src.js
@@ -0,0 +1,4 @@
+// checks if x is an even number
+var isEven = function(x) {
+	return x % 2 === 0;
+};
